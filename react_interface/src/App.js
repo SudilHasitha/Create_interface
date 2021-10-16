e5bdb09bac06eb2c644c5b2065142b3a91047f91
SudilHasitha <it18059564@my.sliit.lk>
2021-10-16 14:50:39 +0530

    react interface creation

---
+++ react_interface/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {BiArchive} from "react-icons/bi"
+
+function App() {
+  return (
+    <div className="App">
+      <h1><BiArchive/></h1>
+    </div>
+  );
+}
+
+export default App;
