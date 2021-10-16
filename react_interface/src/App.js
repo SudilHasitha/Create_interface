4f4fc8dfbf29a491cf9d022ae16bc347d3c566f0
SudilHasitha <it18059564@my.sliit.lk>
2021-10-16 15:12:33 +0530

    rearrange

--- react_interface/src/App.js
+++ react_interface/src/App.js
@@ -4,8 +4,8 @@ import {BiArchive} from "react-icons/bi"
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive/></h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 ><BiArchive/></h1>
     </div>
   );
 }
